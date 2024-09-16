1e4fe0dad89b057b05519678ec5a0025a92c9f97
Pamela Gusmão <134076783+pamelagusmao@users.noreply.github.com>
2024-09-16 17:13:21 -0300

    Update OlaMundo.java

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,18 +1,19 @@
-import.java.util.Scanner;
-
+import java.util.Scanner;
 public class OlaMundo {
- public static void main (String args[]) {
-   System.out.println ("Olá, Mundo!!!:D");
-   Scanner teclado= new Scanner;
-      int num1, num2, result;
-        System.out.println ("informe o primeiro número");
-      num1= teclado.nextInt (); 
-         result=num1+num2;
-        System.out.println ("informe o segundo núumero");
-      num2= teclado.nextInt();
-        System.out.println ("Soma= " (num1+num2));
-        System.out.println ( "Resultado=" + result);
-        teclado.close();
-        System.exit(0);
+ public static void main (String args[]){
+  System.out.println ("Olá, Mundo!!!:D");
+  Scanner teclado= new Scanner (System.in);
+  int num1, num2, result;
+  System.out.println ("informe o primeiro número");
+  num1= teclado.nextInt (); 
+  System.out.println ( "informe o segundo número");
+  num2=teclado.nextInt();
+  result=num1+num2;
+  System.out.println ("Soma= " + result);
+  teclado.close();
+  System.exit(0);
+
+  
+  
  } 
 } 
