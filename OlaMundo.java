e2bbf14c8805638daf982704e03fd0968017e6f8
Pamela Gusmão <pamelaoliveiragusmao@gmail.com>
2024-09-23 15:40:45 -0300

    Commit das mudanças locais em OlaMundo.java antes de fazer o pull

--- OlaMundo.java
+++ OlaMundo.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
 public class OlaMundo {
  public static void main (String args[]){
-   System.out.println ("Olá, Mundo!!!:D");}
-}
+  System.out.println ("Olá, Mundo!!!:D");
+  Scanner teclado= new Scanner (System.in);
+  int num1, num2, result;
+  System.out.println ("informe o primeiro número");
+  num1= teclado.nextInt (); 
+  System.out.println ( "informe o segundo número");
+  num2=teclado.nextInt();
+  result=num1+num2;
+  System.out.println ("Soma= " + result);
+  teclado.close();
+ } 
+} 
