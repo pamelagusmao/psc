0d99b772ef8229eac643cea10f74a2e17a364e53
Pamela Gusmão <pamelaoliveiragusmao@gmail.com>
2024-09-23 15:57:18 -0300

    Adicionar ControledeNotas.java

---
+++ PSC/ControledeNotas.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+  public class ControledeNotas{
+    public static void main(String[]Args){
+        try (Scanner input = new Scanner (System.in)) {
+            int alunoNotaMaior90=0;
+            int alunoReprovado=0;
+            int totalAlunos=0;
+            double somaNotas=0;
+            double maiorNota=Double.NEGATIVE_INFINITY;
+            double menorNota=Double.MAX_VALUE;
+            
+            while (true){
+                System.out.println ("Insira a nota final do aluno (negativa para encerrar):");
+                double nota= input.nextDouble();
+                if (nota<0){
+                    break;
+                }
+                if (nota<0){
+                    break;
+                }
+                if (nota>90){
+                    alunoNotaMaior90++;
+                }
+                if (nota<60){
+                    alunoReprovado++;
+                }
+                totalAlunos++;
+                somaNotas+=nota;
+                if (nota>maiorNota){
+                    maiorNota=nota;
+                }
+                
+                double mediaNotas = totalAlunos > 0 ? somaNotas / totalAlunos:0;
+                
+                System.out.println ("Número de alunos com nota maior que 90"+ alunoNotaMaior90);
+                System.out.println("Número de alunos reprovados (nota>60)"+ alunoReprovado);
+                System.out.println("Número total de alunos:" +totalAlunos);
+                System.out.println("Média das notas:" +mediaNotas);
+                System.out.println("Maior nota:"+ maiorNota);
+                System.out.println("Menor nota"+ menorNota);
+            }  }
+    }
+  }
